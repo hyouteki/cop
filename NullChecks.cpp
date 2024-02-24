ef679a1b9f1f9b4c3048cc313f586b644260c870
Lakshay <lakshay21060@iiitd.ac.in>
2024-02-24 11:38:52 +0530

    updated nullcheck pass

--- NullChecks.cpp
+++ NullChecks.cpp
@@ -29,19 +29,34 @@ namespace {
 		bool runOnFunction(Function &F) override {
 			IRBuilder<> Builder(F.getContext());
 			std::vector<Instruction *> instsToProcess;
-			
+	   
 			BasicBlock *nullBB = BasicBlock::Create(F.getContext(), "NullBB", &F);
 			Builder.SetInsertPoint(nullBB);
-			Type* returnType = F.getReturnType();
+
+			// if `printf` api not already present then declare it
+			Module *M = F.getParent(); 
+			if (!M->getFunction("printf")) {
+				FunctionType *printfType = FunctionType::get(IntegerType::getInt32Ty(F.getContext()),
+															 PointerType::get(Type::getInt8Ty(ctx), 0), true);
+				Function::Create(printfType, Function::ExternalLinkage, "printf", M);
+			}
+			// add a error message in `nullBB`
+            Function *printfFunc = M->getFunction("printf");
+			std::string message = "error: null pointer dereference detected\n";
+            Constant *formatString = Builder.CreateGlobalStringPtr(message);
+            Builder.CreateCall(printfFunc, {formatString});
+			// add a default return type from `nullBB`
+			Type *returnType = F.getReturnType();
 			if (returnType->isVoidTy()) Builder.CreateRetVoid();
 			else if (returnType->isIntegerTy()) Builder.CreateRet(ConstantInt::get(returnType, 0));
-			else if (PointerType* ptrType = dyn_cast<PointerType>(returnType)) {
+			else if (PointerType *ptrType = dyn_cast<PointerType>(returnType)) {
 				if (ptrType->getElementType()->isIntegerTy()) {
 					Builder.CreateRet(ConstantPointerNull::get(ptrType));
 				}
 			}
 			
 			for (BasicBlock &BB: F) {
+				if (&BB == nullBB) continue;
 				for (Instruction &Inst: BB) {
 					if (includeInst(Inst)) instsToProcess.push_back(&Inst);
 				}
@@ -59,27 +74,36 @@ namespace {
 		bool includeInst(Instruction &Inst) {
 			return dyn_cast<StoreInst>(&Inst)
 				|| dyn_cast<LoadInst>(&Inst)
-				|| dyn_cast<GetElementPtrInst>(&Inst);
+				|| dyn_cast<GetElementPtrInst>(&Inst)
+				|| dyn_cast<CallInst>(&Inst);
 		}
 		
 		void processInst(Instruction &Inst, IRBuilder<> &Builder, BasicBlock* nullBB) {
-			Value* basePointer;
+			BasicBlock *currentBB = Inst.getParent();
+			BasicBlock *notNullBB = currentBB->splitBasicBlock(&Inst, "NotNullBB");
+			currentBB->getTerminator()->eraseFromParent();
+			Builder.SetInsertPoint(currentBB);
+			Value *basePointer;
 			if (auto *inst = dyn_cast<StoreInst>(&Inst)) {
 				basePointer = inst->getPointerOperand();
 			} else if (auto *inst = dyn_cast<LoadInst>(&Inst)) {
 				basePointer = inst->getPointerOperand();
 			} else if (auto *inst = dyn_cast<GetElementPtrInst>(&Inst)) {
 				basePointer = inst->getPointerOperand();
+			} else if (auto *inst = dyn_cast<CallInst>(&Inst)) {
+				if (inst->isIndirectCall()) {
+					Value *fnPtr = inst->getCalledOperand();
+					Constant *nullValue = Constant::getNullValue(fnPtr->getType());
+					Value *icmpEqInst = Builder.CreateICmpEQ(fnPtr, nullValue, "IsNull");
+					Builder.CreateCondBr(icmpEqInst, nullBB, notNullBB);
+					return;
+				}
 			} else return;
-			Type* basePointerType = basePointer->getType();
-			PointerType* pointerType = PointerType::get(basePointerType->getPointerElementType(),
+			Type *basePointerType = basePointer->getType();
+			PointerType *pointerType = PointerType::get(basePointerType->getPointerElementType(),
 														basePointerType->getPointerAddressSpace());
-			BasicBlock *currentBB = Inst.getParent();
-			BasicBlock *notNullBB = currentBB->splitBasicBlock(&Inst, "NotNullBB");
-			currentBB->getTerminator()->eraseFromParent();
-			Builder.SetInsertPoint(currentBB);
-			Value* icmpEqInst = Builder.CreateICmpEQ(basePointer, ConstantPointerNull::get(pointerType),
-													 "IsNull");
+			Constant *nullValue = ConstantPointerNull::get(pointerType);
+			Value *icmpEqInst = Builder.CreateICmpEQ(basePointer, nullValue, "IsNull");
 			Builder.CreateCondBr(icmpEqInst, nullBB, notNullBB);
 		}
 	};
